1e72834538a386618b056cea17ae5c06e521b28e
mikas <mikas@mikass-MacBook-Air.local>
2022-05-04 04:42:29 +0300

    update

---
+++ app.js
@@ -0,0 +1,23 @@
+// const express = require('express')
+// const app = express()
+// const cors = require('cors')
+// const mainRouter = require('./router/mainRouter')
+//
+//
+// app.use(express.json())
+// app.use(cors())
+// app.listen(4000)
+//
+// app.use('/', mainRouter)
+
+
+console.log('naujas logas')
+//
+// require('dotenv').config()
+
+// app.use(session({
+//     secret: 'keyboard cat',
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: { secure: false }
+// }))
